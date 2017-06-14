60cba9a925d5c3f30abf8423622c813fcf2d9853
xiaotingamy <502150736@qq.com>
2017-06-14 18:33:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab border-bottom-1px">
+      <router-link to="/goods" tag="div" class="tab-item">商品</router-link>
+      <router-link to="/ratings" tag="div" class="tab-item">评价</router-link>
+      <router-link to="/seller" tag="div" class="tab-item">商家</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import header from '@/components/header/header'
+  export default {
+    components: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import "./common/stylus/mixin.styl"
+#app
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-bottom-1px(rgba(7,17,27,0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      font-size: 14px
+      color: rgb(77,85,93)
+      &.active
+        color: rgb(240,40,40)
+</style>
---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="goods">
+		我是商品
+	</div>
+</template>
+<script>
+	export default {
+
+	}
+</script>
+<style>
+	
+</style>
